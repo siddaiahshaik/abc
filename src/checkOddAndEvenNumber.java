0032148f448b58f76831160de818f13053e296fb
siddaiahshaik <siddashaik.java@gmail.com>
2022-11-15 08:03:53 +0530

    "EvenAndOddNumber"

---
+++ src/checkOddAndEvenNumber.java
@@ -0,0 +1,14 @@
+public class checkOddAndEvenNumber {
+
+    public static void main(String[] args) {
+        int num = 98;
+
+            if (num % 2==0){
+                System.out.println( "number is even number" );
+
+            }else {
+                System.out.println( "number is odd number" );
+            }
+        }
+    }
+
